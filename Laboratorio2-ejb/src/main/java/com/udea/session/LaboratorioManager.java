0d1943a7af70ed352ca928789902d984ecc2a397
Santiago Campuzano <tiago150995@gmail.com>
2018-03-15 22:50:50 -0500

    Facade creado

---
+++ Laboratorio2-ejb/src/main/java/com/udea/session/LaboratorioManager.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.session;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Santiago
+ */
+@Stateless
+public class LaboratorioManager implements LaboratorioManagerLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
